1fc5954a655b5596d72365e26240d819a1e634ef
Paul Debevec <paulmdebevec@gmail.com>
2019-11-05 09:49:02 -0700

    Added total_calories method

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -9,4 +9,20 @@ class Recipe
   def add_ingredient(ingredient, amount)
     @ingredients_required[ingredient] = amount
   end
+
+  def total_calories
+    total_cals = 0
+    ingredient_keys_list = @ingredients_required.keys.flat_map do |ingredient|
+      ingredient.calories
+    end
+    ingredient_values_list = @ingredients_required.values.flat_map do |amount|
+      amount
+    end
+    ingredient_keys_list.each do |ingredient|
+      multiplied_amount = ingredient * ingredient_values_list[0]
+      ingredient_values_list.slice!(0)
+      total_cals += multiplied_amount
+    end
+    total_cals
+  end
 end
