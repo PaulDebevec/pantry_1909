aa7b0723f683d932a9442476e78ff21868a0c25d
Paul Debevec <paulmdebevec@gmail.com>
2019-11-05 11:56:28 -0700

    Added test_it_can_restock

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -24,4 +24,17 @@ class PantryTest < Minitest::Test
     mac_and_cheese.add_ingredient(mac, 8)
     assert_equal 0, pantry.stock_check(cheese)
   end
+
+  def test_it_can_restock
+    pantry = Pantry.new
+    cheese = Ingredient.new("Cheese", "C", 50)
+    mac = Ingredient.new("Macaroni", "oz", 200)
+    mac_and_cheese = Recipe.new("Mac and Cheese")
+    mac_and_cheese.add_ingredient(cheese, 2)
+    mac_and_cheese.add_ingredient(mac, 8)
+    pantry.restock(cheese, 5)
+    assert_equal 5, pantry.stock_check(cheese)
+    pantry.restock(cheese, 10)
+    assert_equal 15, pantry.stock_check(cheese)
+  end
 end
