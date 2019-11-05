5c07a6016b27f784752f0533dd5cae651a524c91
Paul Debevec <paulmdebevec@gmail.com>
2019-11-05 09:20:17 -0700

    Added test_it_adds_ingredient

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -13,5 +13,15 @@ class RecipeTest < Minitest::Test
   def test_it_initializes
     mac_and_cheese = Recipe.new("Mac and Cheese")
     assert_equal "Mac and Cheese", mac_and_cheese.name
+    assert_equal ({}), mac_and_cheese.ingredients_required
+  end
+
+  def test_it_adds_ingredient
+    cheese = Ingredient.new("Cheese", "C", 100)
+    mac = Ingredient.new("Macaroni", "oz", 30)
+    mac_and_cheese = Recipe.new("Mac and Cheese")
+    mac_and_cheese.add_ingredient(cheese, 2)
+    mac_and_cheese.add_ingredient(mac, 8)
+    assert_equal ({cheese => 2, mac => 8}), mac_and_cheese.ingredients_required
   end
 end
