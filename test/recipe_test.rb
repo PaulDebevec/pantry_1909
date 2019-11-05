02f4deafd8724d5cc7619557e2471307de17e2eb
Paul Debevec <paulmdebevec@gmail.com>
2019-11-05 09:15:49 -0700

    Added test_it_initializes

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -6,10 +6,12 @@ require './lib/ingredient'
 class RecipeTest < Minitest::Test
 
   def test_it_exists
-    cheese = Ingredient.new("Cheese", "C", 100)
-    mac = Ingredient.new("Macaroni", "oz", 30)
     mac_and_cheese = Recipe.new("Mac and Cheese")
     assert_instance_of Recipe, mac_and_cheese
   end
-  
+
+  def test_it_initializes
+    mac_and_cheese = Recipe.new("Mac and Cheese")
+    assert_equal "Mac and Cheese", mac_and_cheese.name
+  end
 end
