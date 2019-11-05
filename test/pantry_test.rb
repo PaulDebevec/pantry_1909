e562053e75c390398f91d58d79be5c6db1787471
Paul Debevec <paulmdebevec@gmail.com>
2019-11-05 09:56:21 -0700

    Created PantryTest including test_it_exists, and test_it_can_restock

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -9,4 +9,19 @@ class PantryTest < Minitest::Test
     pantry = Pantry.new
     assert_instance_of Pantry, pantry
   end
+
+  def test_it_initializes
+    pantry = Pantry.new
+    assert_equal Hash.new(0), pantry.stock
+  end
+
+  def test_it_can_restock
+    pantry = Pantry.new
+    cheese = Ingredient.new("Cheese", "C", 50)
+    mac = Ingredient.new("Macaroni", "oz", 200)
+    mac_and_cheese = Recipe.new("Mac and Cheese")
+    mac_and_cheese.add_ingredient(cheese, 2)
+    mac_and_cheese.add_ingredient(mac, 8)
+    assert_equal 0, pantry.stock_check(cheese)
+  end
 end
